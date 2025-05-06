ff1985af890c3e6b6a262684151e2f1cb0479fdf
JOTAROchl <167789565+JOTAROchl@users.noreply.github.com>
2025-05-06 18:37:46 -0400

    Update Autenticacion.java

--- src/main/java/com/example/ConnectForoSPA/model/Autenticacion.java
+++ src/main/java/com/example/ConnectForoSPA/model/Autenticacion.java
@@ -1,5 +1,14 @@
 package com.example.ConnectForoSPA.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Autenticacion {
+
+    private String username;
+    private String password;
 
 }
